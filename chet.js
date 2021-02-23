101f6ae6b181b4f55e7d1f4edef3bf82253b3e9c
Luke Riley <lukeriley@Lukes-iMac-262.attlocal.net>
2021-02-23 15:51:40 -0500

    these are the changes I did to to this document

--- chet.js
+++ chet.js
@@ -1,1 +1,9 @@
-# there will be js in this file
+
+function first() {
+document.body.style.backgroundColor = "greenyellow";
+
+
+
+
+document.getElementById("pear").innerHTML= alert("Click the ok button and the background color will change to greenyellow"); 
+}
